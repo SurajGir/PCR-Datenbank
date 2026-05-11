aa92c82425693d85c85f09a5b78c6bc94e232f61
sgi <sai30492@gmail.com>
2026-05-11 15:40:06 +0200

    Force database migration

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
 
+python manage.py collectstatic --no-input
+
+# These two are the most important right now:
+python manage.py migrate --no-input
 python manage.py createsuperuser --no-input || true
